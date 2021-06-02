c48a6455f121f17029c466d44cb1dad055dfc258
Braulio Consani <brauliomoura@faculdadeimpacta.com.br>
2021-06-01 22:43:14 -0300

    web services

---
+++ src/main/java/br/com/impacta/labs/controllers/DemoRestController.java
@@ -0,0 +1,17 @@
+package br.com.impacta.labs.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+	
+	@GetMapping("/hello")
+	public String digaHello() {
+		return "Hello Spring Rest";
+	}
+	
+
+}
